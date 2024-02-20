9b402397cc64c923c96c5fd0ca60e9fb70c8d3c8
amenrio <71671687+Amenrio@users.noreply.github.com>
2024-02-20 17:17:53 +0100

    docs: added maya documentation dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(0,os.path.abspath('../src'))
-sys.path.insert(1,os.path.abspath("../.config/pylint"))
+sys.path.insert(1,os.path.abspath("/srv/dev/maya_modules"))
 
 # Configuration file for the Sphinx documentation builder.
 #
